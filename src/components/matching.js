28c1975b5ca65499634084f257e929582b7cc087
Chaimae <chaima.aberji@gmail.com>
2024-11-02 01:50:35 +0700

    install Dnd Provider and HTML5Backend to have draggable items for the matching exercise

--- src/components/matching.js
+++ src/components/matching.js
@@ -1,35 +1,54 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "../pages/Quizzes page/quizzes.css";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { useDrag } from "react-dnd";
 
-const Matching = ({ choices }) => {
+const DraggableItem = ({ item }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "answer",
+    item: { item },
+    collect: (monitor) => ({ isdragging: !!monitor.isDragging() }),
+  }));
+  return (
+    <span className="draggable" ref={drag}>
+      {item}
+    </span>
+  );
+};
+
+const Matching = ({ options }) => {
   const [answers, setAnswers] = useState([""]);
   const [questions, setQuestions] = useState([""]);
-  /*
-  const handleChoices = () => {
-    setAnswers(choices.slice(choices.length / 2));
-    setQuestions(choices.slice(0, choices.length / 2 - 1));
-  };
-  */
 
-  useEffect(() => {}, []);
+  const handleChoices = () => {
+    setAnswers(options.slice(options.length / 2));
+    setQuestions(options.slice(0, options.length / 2));
+  };
+
+  useEffect(() => {
+    handleChoices();
+  }, []);
 
   return (
-    <div id="matching">
-      <h3>Matching Activity</h3>
-      <div id="choices-container">
-        <div id="questions">
-          {questions.map((item) => (
-            <span>{item}</span>
-          ))}{" "}
-        </div>
-        <div id="answers">
-          {answers.map((item) => (
-            <span>{item}</span>
-          ))}
+    <DndProvider backend={HTML5Backend}>
+      <div id="matching">
+        <h3>Matching Activity</h3>
+        <div id="choices-container">
+          <div id="questions">
+            {questions.map((item) => (
+              <DraggableItem item={item} />
+            ))}
+          </div>
+          <div id="answers">
+            {answers.map((item) => (
+              <span>{item}</span>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
