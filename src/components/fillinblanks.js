8ed88dab0b769fd50f1053268010558260ab83a9
Chaimae <chaima.aberji@gmail.com>
2024-11-01 23:52:14 +0700

    struggling with fill in the blanks

--- src/components/fillinblanks.js
+++ src/components/fillinblanks.js
@@ -2,27 +2,36 @@ import { useState } from "react";
 import "../pages/Quizzes page/quizzes.css";
 
 const FillInTheBlank = ({ question, options }) => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState([]);
+
+  const handleBlanks = (phrase) => {
+    return phrase.split("_______");
+  };
 
   return (
     <div id="fillblanks">
       <h3>{question}</h3>
-      {options.map((item, index) => (
-        <>
-          <h2>{item}</h2>
-
-          <input
-            type="text"
-            value={input[index]}
-            onChange={(e) => {
-              setInput((prevInput) => {
-                const newInput = [...prevInput];
-                newInput[index] = e.target.value;
-                return newInput;
-              });
-            }}
-          />
-        </>
+      {options.map((item) => (
+        <h2>
+          {handleBlanks(item).map((part, index) => (
+            <>
+              {part}
+              {index < handleBlanks(item).length - 1 && (
+                <input
+                  type="text"
+                  value={input[index]}
+                  onChange={(e) => {
+                    setInput((prevInput) => {
+                      const newInput = [...prevInput];
+                      newInput[index] = e.target.value;
+                      return newInput;
+                    });
+                  }}
+                />
+              )}
+            </>
+          ))}
+        </h2>
       ))}
     </div>
   );
