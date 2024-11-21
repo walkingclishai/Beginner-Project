bc52c194ac76651dbdcca32219777515b931f11c
Chaimae <chaima.aberji@gmail.com>
2024-11-21 23:38:03 +0700

    storing true-false component data

--- src/components/true.false.js
+++ src/components/true.false.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import "../pages/Quizzes page/quizzes.css";
+import { useEffect } from "react";
 
-const TrueFalse = ({ question, options }) => {
+const TrueFalse = ({ question, options, index, storeData }) => {
   const [selected, setSelected] = useState(null);
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    storeData(answers.join(","), index);
+  }, [selected]);
 
   return (
     <div id="tf-container">
       <h4>{question}</h4>
-      {options.map((item) => (
+      {options.map((item, index) => (
         <>
           <h3>{item}</h3>
           <label>
@@ -15,7 +21,12 @@ const TrueFalse = ({ question, options }) => {
               type="radio"
               name={item}
               value="True"
-              onChange={() => setSelected(true)}
+              onChange={() => {
+                setSelected(true);
+                const newAnswers = [...answers];
+                newAnswers[index] = "True";
+                setAnswers(newAnswers);
+              }}
             />
             True
           </label>
@@ -24,7 +35,12 @@ const TrueFalse = ({ question, options }) => {
               type="radio"
               name={item}
               value="False"
-              onChange={() => setSelected(false)}
+              onChange={() => {
+                setSelected(false);
+                const newAnswers = [...answers];
+                newAnswers[index] = "False";
+                setAnswers(newAnswers);
+              }}
             />
             False
           </label>
