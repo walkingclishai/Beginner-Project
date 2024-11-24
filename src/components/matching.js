d52e65f604bdf6ecf640e4f85280a3a3ec2410f7
Chaimae <chaima.aberji@gmail.com>
2024-11-24 23:11:33 +0700

    useDrag and useDrop are hell

--- src/components/matching.js
+++ src/components/matching.js
@@ -6,28 +6,39 @@ import { DndProvider } from "react-dnd";
 import { useDrag } from "react-dnd";
 import { useDrop } from "react-dnd";
 
-const DraggableItem = ({ item }) => {
+const DraggableItem = ({ dragItem }) => {
+  const [item, setItem] = useState({ name: dragItem });
+
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "answer",
-    item: { item },
-    collect: (monitor) => ({ isdragging: !!monitor.isDragging() }),
+    item,
+    collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
   }));
+
+  useEffect(() => {
+    item.name = dragItem;
+  }, [isDragging]);
 
   return (
     <span className="draggable" ref={drag}>
-      {item}
+      {dragItem}
     </span>
   );
 };
 
-const Droppablearea = () => {
+const Droppablearea = ({ handleInput, i }) => {
   const [text, setText] = useState("");
 
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "answer",
-    drop: (item) => setText(item.item),
+    drop: (item) => setText(item.name),
     collect: (monitor) => ({ isOver: !!monitor.isOver() }),
   }));
+
+  useEffect(() => {
+    handleInput(text, i);
+  }, [text]);
+
   return (
     <div className="droppable" ref={drop}>
       {text}
@@ -35,18 +46,28 @@ const Droppablearea = () => {
   );
 };
 
-const Matching = ({ options }) => {
+const Matching = ({ options, index, storeData }) => {
   const [answers, setAnswers] = useState([""]);
   const [questions, setQuestions] = useState([""]);
+  const [input, setInput] = useState([]);
 
   const handleChoices = () => {
     setAnswers(options.slice(options.length / 2));
     setQuestions(options.slice(0, options.length / 2));
   };
+  const handleInput = (txt, i) => {
+    const newInput = [...input];
+    newInput[i] = txt;
+    setInput(newInput);
+  };
 
   useEffect(() => {
     handleChoices();
   }, []);
+
+  useEffect(() => {
+    storeData(input.join(","), index);
+  }, [input]);
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -54,15 +75,17 @@ const Matching = ({ options }) => {
         <h3>Matching Activity</h3>
         <div id="choices-container">
           <div id="questions">
-            {questions.map((item) => (
-              <DraggableItem item={item} />
+            {questions.map((item, j) => (
+              <div key={j}>
+                <DraggableItem dragItem={item} />
+              </div>
             ))}
           </div>
           <div id="answers">
-            {answers.map((item) => (
-              <span id="test5">
+            {answers.map((item, i) => (
+              <span id="test5" key={i}>
                 {item}
-                <Droppablearea />
+                <Droppablearea handleInput={handleInput} i={i} />
               </span>
             ))}
           </div>
