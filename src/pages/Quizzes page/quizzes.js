cd5dab4d804b9f26ec4b2e9016393a9018575061
Chaimae <chaima.aberji@gmail.com>
2024-11-26 10:55:58 +0700

    fix next/submit button

--- src/pages/Quizzes page/quizzes.js
+++ src/pages/Quizzes page/quizzes.js
@@ -41,6 +41,10 @@ function Quizzes() {
     setCurrentIndex(currentIndex + 1);
   };
 
+  const handlePrevious = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
   useEffect(() => {
     fetchQuestions();
   }, []);
@@ -86,6 +90,26 @@ function Quizzes() {
               index={currentIndex}
               storeData={storeData}
             />
+          ) : questions[currentIndex].type == "blanks" ? (
+            <FillInTheBlank
+              question={questions[currentIndex].question}
+              options={questions[currentIndex].choices}
+              index={currentIndex}
+              storeData={storeData}
+            />
+          ) : questions[currentIndex].type == "matching" ? (
+            <Matching
+              options={questions[currentIndex].choices}
+              index={currentIndex}
+              storeData={storeData}
+            />
+          ) : questions[currentIndex].type == "essay" ? (
+            <Essay
+              question={questions[currentIndex].question}
+              options={questions[currentIndex].choices}
+              index={currentIndex}
+              storeData={storeData}
+            />
           ) : (
             <></>
           ))}
@@ -104,7 +128,7 @@ function Quizzes() {
           </>
             ))} */}
         <div id="buttons">
-          <button>
+          <button onClick={handlePrevious}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="3em"
@@ -118,20 +142,38 @@ function Quizzes() {
             </svg>
             Previous
           </button>
-          <button onClick={handleNext}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="3em"
-              height="3em"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="#fed668"
-                d="M12 21c-.801 0-1.555-.312-2.121-.879S8.999 18.8 9 17.998c0-.746.271-.998.764-1.998H4.928c-1.654 0-3-1.347-3-3s1.346-3 3-3h4.836C9.27 9 9 8.745 9 7.999a2.98 2.98 0 0 1 .88-2.121c1.132-1.132 3.108-1.133 4.241.001L21.242 13l-7.121 7.121A2.98 2.98 0 0 1 12 21m-7.072-9a1.001 1.001 0 0 0 0 2h9.658l-3.293 3.293a1 1 0 0 0-.293.706c0 .269.104.519.293.708a1.023 1.023 0 0 0 1.414 0L18.414 13l-5.707-5.707a1.023 1.023 0 0 0-1.414 0a1 1 0 0 0-.293.706c0 .268.104.519.293.708L14.586 12z"
-              ></path>
-            </svg>
-            Next
-          </button>
+
+          {currentIndex === questions.length - 1 ? (
+            <button onClick={() => console.log(data)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="2.5em"
+                height="2.5em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#fed668"
+                  d="M17 3H5c-1.1 0-2 .9-2 2v14a2 2 0 0 0 2 2h6.81c-.39-.66-.64-1.4-.74-2.16a2.994 2.994 0 0 1-1.87-3.81C9.61 13.83 10.73 13 12 13c.44 0 .88.1 1.28.29c2.29-1.79 5.55-1.7 7.72.25V7zm-2 6H5V5h10zm.75 12L13 18l1.16-1.16l1.59 1.59l3.59-3.59l1.16 1.41z"
+                ></path>
+              </svg>
+              Submit
+            </button>
+          ) : (
+            <button onClick={handleNext}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="3em"
+                height="3em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#fed668"
+                  d="M12 21c-.801 0-1.555-.312-2.121-.879S8.999 18.8 9 17.998c0-.746.271-.998.764-1.998H4.928c-1.654 0-3-1.347-3-3s1.346-3 3-3h4.836C9.27 9 9 8.745 9 7.999a2.98 2.98 0 0 1 .88-2.121c1.132-1.132 3.108-1.133 4.241.001L21.242 13l-7.121 7.121A2.98 2.98 0 0 1 12 21m-7.072-9a1.001 1.001 0 0 0 0 2h9.658l-3.293 3.293a1 1 0 0 0-.293.706c0 .269.104.519.293.708a1.023 1.023 0 0 0 1.414 0L18.414 13l-5.707-5.707a1.023 1.023 0 0 0-1.414 0a1 1 0 0 0-.293.706c0 .268.104.519.293.708L14.586 12z"
+                ></path>
+              </svg>
+              Next
+            </button>
+          )}
         </div>
         {/*
             {item.type == "t-f" && (
@@ -176,7 +218,7 @@ function Quizzes() {
           </>
             ))}
 
-        <button onClick={() => console.log(data)}>Submit</button>*/}
+        */}
       </div>
     </>
   );
